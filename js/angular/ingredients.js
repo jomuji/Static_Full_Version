93b7b7038805cc8ad0eda48c101a014c315f777b
José Mujica <jose@netavantage.com>
2015-12-31 11:16:01 -0500

    cleanup 2
    enleve ingredients js

--- js/angular/ingredients.js
+++
@@ -1,5 +0,0 @@
-{"ingredients":[
-    {"id":"1","title":"Chocolat", "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "quantity":"250", "unit":"grams"},
-    {"id":"2", "title":"Bananes", "description":"Smith", "quantity":"350", "unit":"grams"},
-    {"id":"3", "title":"Noix", "description":"Jones", "quantity":"2", "unit":"kilos"}
-]}
